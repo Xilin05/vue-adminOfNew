b9eaa99332086cecc8137caacd2d215f166d480b
fjc <>
2020-12-05 22:58:48 +0800

    feat:取消token验证

--- src/components/common/Home.vue
+++ src/components/common/Home.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="wrapper">
-        <v-head></v-head>
-        <v-sidebar></v-sidebar>
-        <div class="content-box" :class="{'content-collapse':collapse}">
-            <!-- <v-tags></v-tags> -->
-            <div class="content">
-                <transition name="move" mode="out-in">
-                    <keep-alive :include="tagsList">
-                        <router-view></router-view>
-                    </keep-alive>
-                </transition>
-            </div>
-        </div>
+  <div class="wrapper">
+    <v-head></v-head>
+    <v-sidebar></v-sidebar>
+    <div class="content-box" :class="{'content-collapse':collapse}">
+      <!-- <v-tags></v-tags> -->
+      <div class="content">
+        <transition name="move" mode="out-in">
+          <keep-alive :include="tagsList">
+            <router-view></router-view>
+          </keep-alive>
+        </transition>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -21,38 +21,37 @@ import vSidebar from "./Sidebar.vue";
 import vTags from "./Tags.vue";
 import bus from "./bus";
 export default {
-    data() {
-        return {
-            tagsList: [],
-            collapse: false
-        };
-    },
-    components: {
-        vHead,
-        vSidebar,
-        vTags
-    },
-    beforeCreate() {
-        const token = localStorage.getItem("token");
+  data() {
+    return {
+      tagsList: [],
+      collapse: false,
+    };
+  },
+  components: {
+    vHead,
+    vSidebar,
+    vTags,
+  },
+  beforeCreate() {
+    // const token = localStorage.getItem("token");
+    // if (!token) {
+    //     // token 没有 -> 登录
+    //     this.$router.push("/Login");
+    // }
+  },
+  created() {
+    bus.$on("collapse", (msg) => {
+      this.collapse = msg;
+    });
 
-        if (!token) {
-            // token 没有 -> 登录
-            this.$router.push("/Login");
-        }
-    },
-    created() {
-        bus.$on("collapse", msg => {
-            this.collapse = msg;
-        });
-
-        // 只有在标签页列表里的页面才使用keep-alive，即关闭标签之后就不保存到内存中了。
-        bus.$on("tags", msg => {
-            let arr = [];
-            for (let i = 0, len = msg.length; i < len; i++) {
-                msg[i].name && arr.push(msg[i].name);
-            }
-            this.tagsList = arr;
-        });
-    }
+    // 只有在标签页列表里的页面才使用keep-alive，即关闭标签之后就不保存到内存中了。
+    bus.$on("tags", (msg) => {
+      let arr = [];
+      for (let i = 0, len = msg.length; i < len; i++) {
+        msg[i].name && arr.push(msg[i].name);
+      }
+      this.tagsList = arr;
+    });
+  },
 };
 </script>
